d1b33ff6cd006b3453ed1b038b861178f2d6d831
marimain-art <marian.mainarte@gmail.com>
2023-12-10 18:31:45 -0300

    :sparkles: feat: Carrossel da página inicial

--- Site/js/script.js
+++ Site/js/script.js
@@ -1,18 +1,36 @@
+const slider = document.querySelectorAll('.slider');
+const btnPrev = document.getElementById('prev-button');
+const btnNext = document.getElementById('next-button');
 
-var radio = document.querySelector('.manual-btn')
-var cont = 1
+let currentSlide = 0;
 
-document.getElementById('radio1').checked = true
-
-setInterval(() => {
-    nextImg()
-}, 5000)
-
-function nextImg(){
-    cont++
-    if(cont > 4){
-        cont = 1
-    }
-
-    document.getElementById('radio' + cont).checked = true
+function hideSlider() {
+  slider.forEach(item => item.classList.remove('on'))
 }
+
+function showSlider() {
+  slider[currentSlide].classList.add('on')
+}
+
+function nextSlider() {
+  hideSlider()
+  if(currentSlide === slider.length -1) {
+    currentSlide = 0
+  } else {
+    currentSlide++
+  }
+  showSlider()
+}
+
+function prevSlider() {
+  hideSlider()
+  if(currentSlide === 0) {
+    currentSlide = slider.length -1
+  } else {
+    currentSlide--
+  }
+  showSlider()
+}
+
+btnNext.addEventListener('click', nextSlider)
+btnPrev.addEventListener('click', prevSlider)
